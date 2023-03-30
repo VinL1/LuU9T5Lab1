f5ee5670be8a9518078c6b71a10e1abbc87acb30
BT_3W2_05 <vincentl105@Nycstudents.nte>
2023-03-30 08:22:02 -0400

    cow

--- src/Carbaret.java
+++ src/Carbaret.java
@@ -1,32 +1,76 @@
-import java.lang.reflect.Array;
+import java.net.StandardSocketOptions;
 import java.util.ArrayList;
 
-public class Carbaret {
-    private ArrayList<Performer> perform;
+public class Cabaret {
+    private ArrayList<Performer> performers;
     private String name;
 
-    public Carbaret(String name){
-        perform = new ArrayList<Performer>();
+    public Cabaret(String name){
+        performers = new ArrayList<>();
         this.name = name;
     }
 
-    public ArrayList<Performer> getPerform(){
-        return perform;
-    }
-
-    public String getName(){
+    public String getName() {
         return name;
     }
 
+    public ArrayList<Performer> getPerformers() {
+        return performers;
+    }
+
     public boolean addPerformer(Performer performer){
-        if (perform.indexOf(performer) == -1){
-            perform.add(performer);
+        if (!performers.contains(performer)){
+            performers.add(performer);
             return true;
         }
         return false;
     }
 
     public boolean removePerformer(Performer performer){
-        int index = perform.indexOf(performer);
+        if (performers.contains(performer)){
+            performers.remove(performer);
+            return true;
+        }
+        return false;
+    }
+
+    public double averagePerformerAge(){
+        double sum = 0;
+        for (Performer performer: performers){
+            sum += performer.getAge();
+        }
+        return sum/performers.size();
+    }
+
+    public ArrayList<Performer> performersOverAge(int age){
+        ArrayList<Performer> overAge = new ArrayList<>();
+        for (Performer performer : performers){
+            if (performer.getAge() >= age){
+                overAge.add(performer);
+            }
+        }
+        return overAge;
+    }
+
+    public void groupRehearsal(){
+        for (Performer performer: performers){
+            System.out.println("REHEARSAL CALL! "+ performer.getName());
+
+            if (performer instanceof Comedian temp){
+                temp.rehearse(false);
+            }else {
+                performer.rehearse();
+            }
+        }
+    }
+
+    public void cabaretShow(){
+        for (Performer performer: performers){
+            System.out.println("Welcome to the cabaret! Next act up..." + performer.getName());
+            if (performer instanceof Dancer temp){
+                temp.pirouette(2);
+            }
+            performer.perform();
+        }
     }
 }
